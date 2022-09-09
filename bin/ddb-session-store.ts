7a9b5ebc29ef1181a7ca4701ac8ab1ed3de1d6d4
ndilsou <nasoungadoy@gmail.com>
2022-09-09 15:03:30 +0100

    fix cdk env to the sandbox account

--- bin/ddb-session-store.ts
+++ bin/ddb-session-store.ts
@@ -15,4 +15,8 @@ new DdbSessionStoreStack(app, "DdbSessionStoreStack", {
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: {
+    region: "eu-west-1",
+    account: "037131750441",
+  },
 });
